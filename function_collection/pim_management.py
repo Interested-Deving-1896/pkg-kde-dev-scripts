9b9e23e39ce9653f6296769b5e438dcd57a5e8ad
Sandro Knauß <hefee@debian.org>
2020-04-16 23:05:43 +0200

    pim_management: make it the same line frameworks_management.

--- function_collection/pim_management.py
+++ function_collection/pim_management.py
@@ -1,35 +1,18 @@
 from functions import *
 
-for pkg in deb822.Sources.iter_paragraphs(open('/home/hefee/debian/graph/Sources')):
-    if pkg['Package'] not in packages:
-        packages[pkg['Package']] = Package(pkg)
+from functions import *
+import salsa
+
+kdedir = basedir/"kde"
 
 #Read tier data
 tiers=[]
-for subgraph in pydot.graph_from_dot_file("/home/hefee/debian/graph/tier.dot")[0].get_subgraph_list():
+dotpath = (os.path.join(os.path.dirname(__file__),'kdepim.tier.dot'))
+for subgraph in pydot.graph_from_dot_file(dotpath)[0].get_subgraph_list():
     tier=set()
     for node in subgraph.get_nodes():
-        tier.add(packages[node.get_name()[1:-1]])
+        pkg_name = node.get_name()[1:-1]
+        pkg_path = kdedir/pkg_name
+        control = pkg_path/"debian/control"
+        tier.add(getPackage(control))
     tiers.append(tier)
-
-#endlist for 17.12
-endlist = set(['kblog', 'knotes','kdepim-runtime','kmail-account-wizard','pim-sieve-editor','kontact','kaddressbook','akregator','grantlee-editor', 'akonadiconsole','akonadi-calendar-tools','kalarm','kmail','mbox-importer','pim-data-exporter','kdepim-addons','korganizer', 'kgpg', 'kleopatra'])
-
-endlist = set([packages[i] for i in endlist])
-
-# for pkg in tiers[6]:
-#     if pkg in endlist:
-#         continue
-#     print(pkg.name)
-#     bumpCompat(pkg, 10)
-#     pkg.dpkgBuildpackage()
-#     dput(pkg)
-
-
-# for pkg in packages.values():
-#     with tempfile.NamedTemporaryFile() as tmpfile:
-#         if checkForSymbolChanges(pkg, tmpfile.name):
-#             version = (f"{pkg.changelog.epoch}:"if pkg.changelog.epoch else "") + str(pkg.changelog.versions[0].upstream_version)
-#             if updateSymbols(pkg, version, [tmpfile.name]) is None:
-#                 pkg.dpkgBuildpackage()
-#                 dput(pkg)
